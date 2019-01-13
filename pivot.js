2e0ca49190ffa4f384a53030adc97be11ccc984b
ElementG9 <elementg9dev@gmail.com>
2019-01-12 20:44:07 -0700

    Make Pivot code to run more accessible.

--- pivot.js
+++ pivot.js
@@ -1,9 +1,12 @@
+// The Pivot code to run.
+var code = `asdf = (a) {return(a++)}`;
+
 // Import the tokenizer and parser.
 const tokenize = require("./tokenizer.js");
 const parse = require("./parser.js");
 
 // Generate the AST.
-var ast = parse(tokenize(`asdf = (a) {return(a++)}`));
+var ast = parse(tokenize(code));
 
 // Write the AST to ast.json.
 var fs = require("fs");
