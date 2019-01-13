59fe22b51246ea54b38b0ca9391aeb9a503f3d6b
ElementG9 <elementg9dev@gmail.com>
2019-01-12 20:13:24 -0700

    Publish current work
    The work that has already been done is being published in a GitHub repo.

---
+++ rpn.js
@@ -0,0 +1,40 @@
+var solve = (exp) => {
+    var stack = [];
+    var expression = exp.split(" ");
+    for(var j=0;j<expression.length;j++) {
+        var key = expression[j];
+        if(key.match(/\d/)) {
+            stack.push(parseInt(key));
+        } else if(key.match(/\w/)) {
+            if(Object.keys(progData).includes(key)) {
+                stack.push(progData[key]);
+            } else {
+                stack.push(key);
+            }
+        }
+        switch(key) {
+            case "+": // add
+                var opItems = stack.splice(stack.length-2,2);
+                var result = opItems[0]+opItems[1];
+                stack.push(result);
+            break;
+            case "-": // subtract
+                var opItems = stack.splice(stack.length-2,2);
+                var result = opItems[0]-opItems[1];
+                stack.push(result);
+            break;
+            case "*": // multiply
+                var opItems = stack.splice(stack.length-2,2);
+                var result = opItems[0]*opItems[1];
+                stack.push(result);
+            break;
+            case "/": // divide
+                var opItems = stack.splice(stack.length-2,2);
+                var result = opItems[0]/opItems[1];
+                stack.push(result);
+            break;
+        }
+    }
+    return stack;
+};
+module.exports = solve;
